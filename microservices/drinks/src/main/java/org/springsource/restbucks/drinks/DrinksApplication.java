1723f361de8252d979197ccebdbd3dcf826743f0
Cora Iberkleid <ciberkleid@gmail.com>
2025-05-19 15:38:14 -0400

    Add drinks microservice

---
+++ microservices/drinks/src/main/java/org/springsource/restbucks/drinks/DrinksApplication.java
@@ -0,0 +1,13 @@
+package org.springsource.restbucks.drinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinksApplication.class, args);
+    }
+
+}
---
+++ microservices/drinks/src/test/java/org/springsource/restbucks/drinks/DrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springsource.restbucks.drinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
